0d385b46f78c85cd03f121eca1e6a710e0deb9fd
Sirinoot Ketkham(Fukfang) <98077546+SirinootKK@users.noreply.github.com>
2023-03-07 15:31:41 +0700

    add new path for return json object

--- temple/temple/settings.py
+++ temple/temple/settings.py
@@ -130,3 +130,27 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
+INSTALLED_APPS = [
+'django.contrib.admin',
+'django.contrib.auth',
+'django.contrib.contenttypes',
+'django.contrib.sessions',
+'django.contrib.messages',
+'django.contrib.staticfiles',
+'rest_framework',
+'rest_framework.authtoken',
+'temple_api',
+'corsheaders',
+]
+
+MIDDLEWARE = [
+'django.middleware.security.SecurityMiddleware',
+'django.contrib.sessions.middleware.SessionMiddleware',
+'django.middleware.common.CommonMiddleware',
+'django.middleware.csrf.CsrfViewMiddleware',
+'django.contrib.auth.middleware.AuthenticationMiddleware',
+'django.contrib.messages.middleware.MessageMiddleware',
+'django.middleware.clickjacking.XFrameOptionsMiddleware',
+'corsheaders.middleware.CorsMiddleware',
+'django.middleware.common.CommonMiddleware',
+]
--- temple/temple_api/views.py
+++ temple/temple_api/views.py
@@ -8,17 +8,22 @@ import re
 import csv
 # Create your views here.
 class GetTempleAPIView(APIView):
-    def post(self,request, format=None):
-        body = request.body
-        body_load = json.loads(body)
-        body_list = body_load["body"]
-        print(body_list)
+
+    def _get_province(self):
         provinces = {
             "ROY": "https://th.wikipedia.org/wiki/รายชื่อวัดในจังหวัดร้อยเอ็ด",
             "RAN": "https://th.wikipedia.org/wiki/รายชื่อวัดในจังหวัดระนอง",
             "RAY": "https://th.wikipedia.org/wiki/รายชื่อวัดในจังหวัดระยอง",
             "YAL": "https://th.wikipedia.org/wiki/รายชื่อวัดในจังหวัดยะลา"
         }
+        return provinces
+
+    def post(self,request, format=None):
+        body = request.body
+        body_load = json.loads(body)
+        body_list = body_load["body"]
+        print(body_list)
+        provinces = self._get_province()
         all_temple_list = []
         for p in body_list:
             print(p)
@@ -44,4 +49,41 @@ class GetTempleAPIView(APIView):
         response['Content-Disposition'] = 'attachment; filename="templev1.csv.csv"'
         writer = csv.writer(response, delimiter=' ')
         writer.writerows([[temple.replace("\"",'').replace(" ",'')] for temple in all_temple_list_ex_dup])
+        return response
+    
+    def get(self,request):
+        query_province = request.GET.get('p', '').split(',')
+        print(query_province)
+        provinces = self._get_province()
+        temples_obj = {}
+        for p in query_province:
+            all_temple_list = []
+            print(p)
+            data = requests.get(provinces[p])
+            pattern = r'<div id="mw-content-text" class="mw-body-content.*id="ดูเพิ่ม">ดูเพิ่ม</span>'
+            result = re.findall(pattern, data.text,flags=re.DOTALL | re.MULTILINE)
+            pattern = r'<li>.*</li>'
+            result = re.findall(pattern,result[0] )
+            result = re.sub('<.*?>','', '\n'.join(result))
+            pattern = r'วัด.*'
+            result = re.findall(pattern, result)
+            result = re.sub(' .*','','\n'.join(result))
+            print(result)
+            temples = result.split('\n')
+            for temple in temples:
+                all_temple_list.append(temple)
+            print(all_temple_list)
+            temples_obj[p] = all_temple_list
+        temple_obj_removed_dup = {}
+        for province in temples_obj.items():
+            _l = []
+            p = list(province)
+            for temple in p[1]:
+                if temple not in _l:
+                    _l.append(temple)
+            temple_obj_removed_dup[p[0]] = _l
+        responseData = {}
+        responseData["status"] = 200
+        responseData["data"] = temple_obj_removed_dup
+        response = HttpResponse(content_type='application/json',content=json.dumps(responseData))
         return response
